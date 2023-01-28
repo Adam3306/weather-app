74a686d1b1962aab6e14d13326ec203eb1518e3b
Adam Magyar <adam.magyar@vodafone.com>
2023-01-28 16:55:13 +0100

    refactor: add clock and util function

--- src/features/weather/Weather.jsx
+++ src/features/weather/Weather.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import CircleLoader from "react-spinners/CircleLoader";
 
 import { useGetWeatherQuery } from "./weatherSlice";
+import { Clock } from "../../components";
+import { timeStampToHourMinute } from "../../utils";
+
 export default function Weather({ city }) {
   const { data, isLoading, isSuccess, isError, error } =
     useGetWeatherQuery(city);
-  console.log("error\t", error);
+
   if (isLoading) {
     // TODO: move to the middle
     return <CircleLoader color="#36d7b7" />;
@@ -26,20 +29,14 @@ export default function Weather({ city }) {
       main: { temp },
       sys: { sunrise, sunset },
     } = data;
-    console.log(new Date(sunrise * 1000).getHours());
-    console.log(new Date(sunset * 1000).getMinutes());
+
     return (
       <div>
+        <Clock />
         <p>{city}</p>
         <p>{temp}C</p>
-        <p>
-          {new Date(sunrise * 1000).getHours()}:
-          {new Date(sunrise * 1000).getMinutes()}
-        </p>
-        <p>
-          {new Date(sunset * 1000).getHours()}:
-          {new Date(sunset * 1000).getMinutes()}
-        </p>
+        <p>{timeStampToHourMinute(sunrise)}</p>
+        <p>{timeStampToHourMinute(sunset)}</p>
       </div>
     );
   }
